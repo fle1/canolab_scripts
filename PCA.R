661100f1b05efbe9f0c21a2b5075151d3a698995
fle1 <40067529+fle1@users.noreply.github.com>
2020-09-21 15:21:51 +0200

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -9,20 +9,20 @@
 
 # Define ploting function
 
-myPCA.plot <- function (PCA, main, col = rep(1,7)) {
+myPCA.plot <- function (PCA, main, col = rep(1,7), cex = 1, cex.text = 1, pch = 4) {
   P <- PCA$loadings
   TOTvar <- cumsum(PCA$sdev^2/sum(PCA$sdev^2))
   varPC1 <- round(TOTvar[1] * 100)
   varPC2 <- round((TOTvar[2] - TOTvar[1])*100) 
   plot (P[,1], P[,2], 
-        col = "grey", pch=4,
-        main = main, 
+        col = "grey", pch=pch,
+        main = main, cex=cex,
         xlim = c(range(P[,1])[1]* 1.05,range(P[,1])[2]*0.95),
         ylim = range(P[,2]) * 1.25,
         xlab = paste("PC1: ", varPC1, "% expl.var.", sep = ""),
         ylab = paste("PC2: ", varPC2, "% expl.var.", sep = "")
   ) ## plot sample values for the two first PCs
   text (P[,1], P[,2], rownames(P), offset = 0,
-        col = col, cex = 0.9
+        col = col, cex = cex.text
   )
 }
