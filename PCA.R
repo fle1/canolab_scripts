d8424a0e70ef589e7aca98d0a912f9d7ecbe1961
fle1 <40067529+fle1@users.noreply.github.com>
2020-09-21 15:38:24 +0200

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -17,8 +17,8 @@ myPCA.plot <- function (PCA, main, col = rep(1,7), cex = 1, cex.text = 1, pch = 
   plot (P[,1], P[,2], 
         col = "grey", pch=pch,
         main = main, cex=cex,
-        xlim = range(P[,1]) * 1.25,
-        ylim = range(P[,2]) * 1.25,
+        xlim = range(P[,1]),
+        ylim = range(P[,2]),
         xlab = paste("PC1: ", varPC1, "% expl.var.", sep = ""),
         ylab = paste("PC2: ", varPC2, "% expl.var.", sep = "")
   ) ## plot sample values for the two first PCs
