8ba5705c6498003f87daac7c36ac63551edcf053
fle1 <40067529+fle1@users.noreply.github.com>
2021-12-14 11:29:13 +0100

    Add files via upload
    Color palettes from NatComm

---
+++ color_palettes.R
@@ -0,0 +1,22 @@
+###################################
+##  Color palettes               ##
+##  (Fàbregas et al., 2018)      ##
+##  Fidel Lozano-Elena           ##
+##  CRAG - 16/01/2020            ##
+###################################
+
+
+# Description of the script ----
+
+# Set of color palettes deployed in Fàbregas N., Lozano-Elena F., et al (2018). Nature communications
+
+
+# Continous scale (From Blue to Red, eg. For ploting FC)
+library(gplots)
+mycolors<-colorpanel(400,low="#08306b",high="#c71515",mid ="#ffffff")
+
+# 5 Greens palette
+mypalette<-c("#008080","#329999","#66b2b2","#99cccc","#cce5e5")
+
+# Red
+myred<-"#C71515"
